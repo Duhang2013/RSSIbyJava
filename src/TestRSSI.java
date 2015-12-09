60fb55eaeb63c02a805a5c15c131c3e2c51ae0fc
Duhang2013 <duhang@whut.edu.cn>
2015-12-09 16:40:07 +0800

    complete singlePoint twoPoints threePoints

--- src/TestRSSI.java
+++ src/TestRSSI.java
@@ -1,33 +1,247 @@
 import java.util.HashMap;
 import java.util.Map;
 
+import util.Matrix;
 
 public class TestRSSI {
-	
-	public final float A = 47.0f;
-	public final float n = 4;
 
-	
+	public final static float A = 47.0f;
+	public final static float n = 4;
+
 	public static void main(String[] args) {
+		// num表示有几个天线的RSSI值不为0
+		int num = 0;
+
+		// 以5根天线为例
+		Map<Object, String> info1 = new HashMap<Object, String>();
+		Map<Object, String> info2 = new HashMap<Object, String>();
+		Map<Object, String> info3 = new HashMap<Object, String>();
+		Map<Object, String> info4 = new HashMap<Object, String>();
+		Map<Object, String> info5 = new HashMap<Object, String>();
 		
-		//模拟数据
-		Map<Object, Object> info = new HashMap<Object, Object>();
-		info.put("t", 12);
-		info.put("EPC", 12);
-		info.put("ID", 1);
-		info.put("RSSI", 60);
-		
-		
+		Map<String, Map<Object, String>> info = new HashMap<String, Map<Object, String>>();
+
+		// 分别给天线的参数赋值
+		info.put("1", info1);
+		info.put("2", info2);
+		info.put("3", info3);
+		info.put("4", info4);
+		info.put("5", info5);
+
+		info1.put("ID", "1");
+		info1.put("t", "12");
+		info1.put("EPC", "12");
+		info1.put("RSSI", "10");
+		info1.put("x", "0");
+		info1.put("y", "600");
+
+		info2.put("ID", "2");
+		info2.put("t", "12");
+		info2.put("EPC", "12");
+		info2.put("RSSI", "10");
+		info2.put("x", "0");
+		info2.put("y", "2700");
+
+		info3.put("ID", "3");
+		info3.put("t", "12");
+		info3.put("EPC", "12");
+		info3.put("RSSI", "0");
+		info3.put("x", "7000");
+		info3.put("y", "600");
+
+		info4.put("ID", "4");
+		info4.put("t", "12");
+		info4.put("EPC", "12");
+		info4.put("RSSI", "0");
+		info4.put("x", "7000");
+		info4.put("y", "4800");
+
+		info5.put("ID", "5");
+		info5.put("t", "12");
+		info5.put("EPC", "12");
+		info5.put("RSSI", "1000");
+		info5.put("x", "7000");
+		info5.put("y", "2700");
+
+		// 得到RSSI值的字符串数组
+		String[] Rssi = new String[] { info1.get("RSSI"), info2.get("RSSI"),
+				info3.get("RSSI"), info4.get("RSSI"), info5.get("RSSI") };
+
+		String[] jud = new String[Rssi.length];
+
+		int i = 0;
+		for (String s : Rssi) {
+			i++;
+			if (!s.equals("0")) {
+				jud[num] = String.valueOf(i);// 得到RSSI不为0天线序号的数组
+				num++;
+			}
+		}
+
+		// 将字符串数组按照字符串中数字的大小排序
+		int[] ia = new int[Rssi.length];
+		for (int j = 0; j < Rssi.length; j++) {
+			ia[j] = Integer.parseInt(Rssi[j]);
+		}
+		sort(ia);
+
+		String[] Rssi1 = new String[ia.length];
+		for (int j = 0; j < ia.length; j++) {
+			Rssi1[j] = String.valueOf(ia[j]);
+		}
+
+		String[] jud1 = new String[Rssi.length * info.size()];
+		int n = 0;
+		for (int m = 0; m < Rssi1.length; m++) {
+			for (int j = 1; j <= Rssi1.length; j++) {
+				if ((info.get(String.valueOf(j)).get("RSSI").equals(Rssi1[m]))
+						&& !(info.get(String.valueOf(j)).get("RSSI")
+								.equals("0"))) {
+					jud1[n] = String.valueOf(j);
+					n++;
+				}
+			}
+		}
+
+		switch (num) {
+		case 0:
+			System.out.println("船上未感应到任何物体");
+			break;
+		case 1:
+			System.out.println("有一根天线采集到了数据");
+			singlePoint(info.get(jud1[0]));
+			break;
+		case 2:
+			System.out.println("有两根天线采集到了数据");
+			twoPoints(info.get(jud1[0]), info.get((jud1[1])));
+			break;
+		default:
+			System.out.println("三根及以上天线采集到了数据");
+			System.out.println(info.get(jud1[0]) + "\n" + info.get(jud1[1])
+					+ "\n" + info.get(jud1[2]));
+			threePoints(info.get(jud1[0]), info.get(jud1[1]), info.get(jud1[2]));
+		}
+	}
+	
+	/*
+	 * 胡路瑶
+	 * 功能：排序函数
+	 */
+	private static void sort(int[] a) {
+		 for (int i = 0; i < a.length-1; i++) {
+			 for (int j = i+1; j < a.length; j++) {
+				 if(a[j]>a[i]){
+					 int t=a[j];
+					 a[j]=a[i];
+					 a[i]=t;
+				 }
+			 }
+		 }
+	}
+	
+	/*
+	 * 胡路瑶
+	 * 功能：实现单点定位 
+	 * 接收参数：map(包含：t EPC ID RSSI x y )
+	 * 返回参数：
+	 */
+	private static void singlePoint(Map map) {
+		System.out.println("EPC为："+ map.get("EPC")+"的横坐标为"+map.get("x")+
+				"，纵坐标为"+ map.get("y")+",感应到的时间为"+ map.get("t")); 
+	}
+
+	/*
+	 * 胡路瑶
+	 * 功能：实现两点定位 
+	 * 接收参数：map1、map2(包含：t EPC ID RSSI x y )
+	 * 返回参数：
+	 */
+	private static void twoPoints(Map map1,Map map2) {
+		if(Integer.parseInt( (String) map1.get("RSSI"))>=2*Integer.parseInt((String) map2.get("RSSI"))){
+			System.out.println("EPC为："+ map1.get("EPC")+"的横坐标为"+map1.get("x")+
+					"，纵坐标为"+ map1.get("y")+",感应到的时间为"+ map1.get("t"));
+		}
+		else if(2*Integer.parseInt((String) map1.get("RSSI"))<=Integer.parseInt((String) map2.get("RSSI"))){
+		System.out.println("EPC为："+ map2.get("EPC")+"的横坐标为"+map2.get("x")+
+				"，纵坐标为"+ map2.get("y")+",感应到的时间为"+ map2.get("t")); 
+		}
+		else{ 
+			System.out.println("EPC为："+ map2.get("EPC")+
+			"的横坐标为"+(Integer.parseInt((String) map2.get("x"))+Integer.parseInt((String) map1.get("x")))/2+
+			"，纵坐标为"+(Integer.parseInt((String) map2.get("y"))+Integer.parseInt((String) map1.get("y")))/2+
+			",感应到的时间为"+ map2.get("t")); 
+		}
+	}
+	
+	/*
+	 * 杜航
+	 * 功能：利用公式 RSSI = -(A + 10*n*lg(d)) 求解 d
+	 * 接收参数：RSSI(包含：t EPC ID RSSI x y )
+	 * 返回参数：d
+	 */
+	public static float rssiFormula(float RSSI) {
+
+		float d = (float) Math.exp(-(RSSI + A) / (10 * n));
+		return d;
 	}
 	
 	/*
 	 * 杜航
 	 * 功能：实现RSSI三点定位 
-	 * 接收参数：receive包含：t EPC ID RSSI 
-	 * 返回参数：
+	 * 接收参数：receive1、receive2、receive3(包含：t EPC ID RSSI x y )
+	 * 返回参数：result
 	 */
-	public void threePoints() {
+	public static Map threePoints(Map receive1, Map receive2, Map receive3) {
 		
+		Map<Object, Object> result = new HashMap<Object, Object>();//返回参数放在result中
+		
+		float RSSI1, RSSI2, RSSI3, x1, x2, x3, y1, y2, y3, d1, d2, d3, A1, A2, A3, A4, B1, B2;
+		
+		RSSI1 = Float.parseFloat(receive1.get("RSSI").toString());
+		RSSI2 = Float.parseFloat(receive2.get("RSSI").toString());
+		RSSI3 = Float.parseFloat(receive3.get("RSSI").toString());
+		
+		x1 = Float.parseFloat(receive1.get("x").toString());
+		x2 = Float.parseFloat(receive2.get("x").toString());
+		x3 = Float.parseFloat(receive3.get("x").toString());
+		
+		y1 = Float.parseFloat(receive1.get("y").toString());
+		y2 = Float.parseFloat(receive2.get("y").toString());
+		y3 = Float.parseFloat(receive3.get("y").toString());
+		
+		d1 = rssiFormula(RSSI1);
+		d2 = rssiFormula(RSSI2);
+		d3 = rssiFormula(RSSI3);
+		
+		//封装矩阵的元素
+		A1 = 2 * (x1 - x3);
+		A2 = 2 * (y1 - y3);
+		A3 = 2 * (x2 - x3);
+		A4 = 2 * (y2 - y3);
+		
+		B1 = (float) (Math.pow(x1,2) - Math.pow(x3,2) + Math.pow(y1,2) - Math.pow(y3,2) + Math.pow(d3,2) - Math.pow(d1,2));
+		B2 = (float) (Math.pow(x2,2) - Math.pow(x3,2) + Math.pow(y2,2) - Math.pow(y3,2) + Math.pow(d3,2) - Math.pow(d2,2));
+		
+		float tempA[][] = { { A1, A2 }, { A3, A4 } };
+		Matrix A = new Matrix(tempA);
+		
+		float tempB[][] = { { B1 }, { B2 } };
+		Matrix B = new Matrix(tempB);
+		
+		System.out.println("A:\n" + A.toString());
+		System.out.println("B:\n" + B.toString());
+		
+		Matrix XY = new Matrix(1, 2);//存放求解出的x,y坐标
+		
+		System.out.println("A的逆矩阵:\n" + A.reverse().toString());
+
+		XY = A.reverse().multi(B);
+		
+		
+		System.out.println("XY:\n" + XY.toString());
+		
+		
+		return result;
 	}
 
 	
--- src/util/Matrix.java
+++ src/util/Matrix.java
@@ -1,42 +1,37 @@
 package util;
 
 /**
- * 作者：杜航 
- * 功能：Matrix类实现矩阵的基本操作
+ * 作者：杜航 功能：Matrix类实现矩阵的基本操作
  */
 public class Matrix {
 
 	private float value[][]; // 存储矩阵元素的二维数组（选用float类型）
 
 	/*
-	 * 杜航 
-	 * 功能： 构造m行n列的空矩阵
+	 * 杜航 功能： 构造m行n列的空矩阵
 	 */
 	public Matrix(int m, int n) {
 		this.value = new float[m][n];
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 构造n行n列的空矩阵
+	 * 杜航 功能： 构造n行n列的空矩阵
 	 */
 	public Matrix(int n) {
 		this(n, n);
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 无参构造函数默认构造2行2列的空矩阵
+	 * 杜航 功能： 无参构造函数默认构造2行2列的空矩阵
 	 */
 	public Matrix() {
 		this(2, 2);
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 构造矩阵，由数组mat提供矩阵元素
+	 * 杜航 功能： 构造矩阵，由数组mat提供矩阵元素
 	 */
-	public Matrix(int mat[][]) {
+	public Matrix(float mat[][]) {
 		this(mat.length, mat[0].length);
 		for (int i = 0; i < mat.length; i++)
 			for (int j = 0; j < mat[i].length; j++)
@@ -44,24 +39,21 @@ public class Matrix {
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 获得矩阵第i行第j列的元素
+	 * 杜航 功能： 获得矩阵第i行第j列的元素
 	 */
 	public float get(int i, int j) {
 		return value[i][j];
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 设置矩阵第i行第j列的元素
+	 * 杜航 功能： 设置矩阵第i行第j列的元素
 	 */
 	public void set(int i, int j, int k) {
 		value[i][j] = k;
 	}
 
 	/*
-	 * 杜航
-	 * 功能： 行主序遍历，访问矩阵全部元素
+	 * 杜航 功能： 行主序遍历，访问矩阵全部元素
 	 */
 	public String toString() {
 		String str = "";
@@ -74,8 +66,7 @@ public class Matrix {
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： this和b两个矩阵相加，改变当前矩阵
+	 * 杜航 功能： this和b两个矩阵相加，改变当前矩阵
 	 */
 	public void add(Matrix b) {
 		for (int i = 0; i < this.value.length; i++)
@@ -84,8 +75,7 @@ public class Matrix {
 	}
 
 	/*
-	 * 杜航 
-	 * 功能：this和b两个矩阵相减，改变当前矩阵
+	 * 杜航 功能：this和b两个矩阵相减，改变当前矩阵
 	 */
 	public void minus(Matrix b) {
 		for (int i = 0; i < this.value.length; i++)
@@ -94,13 +84,12 @@ public class Matrix {
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 矩阵相乘：当前矩阵=当前矩阵*b
+	 * 杜航 功能： 矩阵相乘：当前矩阵=当前矩阵*b
 	 */
-	public void multi(Matrix b) {
+	public Matrix multi(Matrix b) {
 		if (this.value[0].length != b.value.length) {
 			System.out.println("这两个矩阵不能相乘");
-			return;
+			return null;
 		}
 		Matrix temp = new Matrix(value.length, b.value[0].length);
 		for (int i = 0; i < value.length; i++)
@@ -110,11 +99,12 @@ public class Matrix {
 					temp.value[i][j] += value[i][k] * b.value[k][j];
 			}
 		this.value = temp.value;
+
+		return this;
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 返回转置矩阵
+	 * 杜航 功能： 返回转置矩阵
 	 */
 	public Matrix transpose() {
 		Matrix temp = new Matrix(value[0].length, value.length);
@@ -125,8 +115,7 @@ public class Matrix {
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 返回伴随矩阵
+	 * 杜航 功能： 返回伴随矩阵
 	 */
 	private Matrix ajoint() {
 		int m = this.value.length;
@@ -134,29 +123,36 @@ public class Matrix {
 
 		Matrix temp = new Matrix(m, n);
 
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				if ((i + j) % 2 == 0) {
-					temp.value[i][j] = this.detCofactor(i, j).detValue();
-				} else {
-					temp.value[i][j] = -this.detCofactor(i, j).detValue();
+		if (m > 2) {
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					if ((i + j) % 2 == 0) {
+						temp.value[i][j] = this.detCofactor(i, j).detValue();
+					} else {
+						temp.value[i][j] = -this.detCofactor(i, j).detValue();
+					}
 				}
 			}
+			return temp.transpose();
+		} else {
+			temp.value[0][0] = this.value[1][1];
+			temp.value[0][1] = -this.value[0][1];
+			temp.value[1][0] = -this.value[1][0];
+			temp.value[1][1] = this.value[0][0];
+
+			return temp;
 		}
-		return temp.transpose();
 	}
 
 	/*
-	 * 杜航 
-	 * 功能： 返回行列式的代数余子式
+	 * 杜航 功能： 返回行列式的代数余子式
 	 */
 	private Matrix detCofactor(int i, int j) {
 
 		int x = this.value.length; // 输入矩阵的行数列数
 		int y = this.value[0].length;
 
-		Matrix temp = new Matrix(this.value[0].length - 1,
-				this.value.length - 1);// b为所求的余子式
+		Matrix temp = new Matrix(x - 1, y - 1);// b为所求的余子式
 
 		for (int m = 0; m < x - 1; m++) {
 			if (m < i) {
@@ -183,8 +179,7 @@ public class Matrix {
 	}
 
 	/*
-	 * 杜航 
-	 * 功能：求解矩阵（行列式）的模值
+	 * 杜航 功能：求解矩阵（行列式）的模值
 	 */
 	public float detValue() {
 		float val = 0;
@@ -193,23 +188,21 @@ public class Matrix {
 			val = this.value[0][0] * this.value[1][1] - this.value[0][1]
 					* this.value[1][0];
 		} else {
-			for (int i = 0; i < this.value[0].length; i++) {
-				/* 若矩阵不为2*2那么需求出矩阵第一行代数余子式的和 */
+			for (int i = 0; i < this.value[0].length; i++) {// 若矩阵不为2*2那么需求出矩阵第一行代数余子式的和
 				Matrix temp = this.detCofactor(0, i);
-				if (i % 2 == 0) {
-					/* 递归 */
+				if (i % 2 == 0) { // 递归
 					val = val + this.value[0][i] * temp.detValue();
 				} else {
 					val = val - this.value[0][i] * temp.detValue();
 				}
 			}
 		}
+
 		return val;
 	}
 
 	/*
-	 * 杜航 
-	 * 功能：求n*n矩阵的逆矩阵
+	 * 杜航 功能：求n*n矩阵的逆矩阵
 	 */
 	public Matrix reverse() {
 		int m = this.value.length;
@@ -217,48 +210,66 @@ public class Matrix {
 
 		Matrix temp = new Matrix(m, n);
 
-		temp = this.ajoint();
-		for (int i = 0; i < m; i++) {
-			for (int j = 0; j < n; j++) {
-				temp.value[i][j] = temp.value[i][j] / this.detValue();
+		float value = this.detValue(); // 模值
+		if (value != 0) {
+			temp = this.ajoint(); // 伴随矩阵
+			for (int i = 0; i < m; i++) {
+				for (int j = 0; j < n; j++) {
+					temp.value[i][j] = temp.value[i][j] / value;
+				}
 			}
+			return temp;
+		} else {
+			System.out.println("行列式的模值为0！不存在逆矩阵！！！");
+			return null;
 		}
-
-		return temp;
 	}
 
 	/*
-	 * 杜航 
-	 * 功能：main()测试矩阵的基本操作函数
+	 * 杜航 功能：main()测试矩阵的基本操作函数
 	 */
 	public static void main(String args[]) {
-		// int m1[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
-		// Matrix a = new Matrix(m1);
-		// int m2[][] = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
-		// Matrix b = new Matrix(m2);
-		// System.out.print("Matrix a:\n" + a.toString());
-		// System.out.print("Matrix b:\n" + b.toString());
-		// a.add(b);
-		// a.multi(b);
-		// System.out.print("Matrix a*b:\n" + a.toString());
-		// System.out.println("a的转置矩阵：\n"+a.transpose().toString());
 
-		// int m3[][] = { { 1, 2, -3 }, { 0, 1, 2 }, { 0, 0, 1 } };
-		// Matrix c = new Matrix(m3);
-		// System.out.println("Value of det:\n" +
-		// c.detCofactor(3,3).toString());
+		float m1[][] = { { 1, 2, 3 }, { 4, 5, 6 } };
+		Matrix a = new Matrix(m1);
+		float m2[][] = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+		Matrix b = new Matrix(m2);
+		System.out.print("Matrix a:\n" + a.toString());
+		System.out.print("Matrix b:\n" + b.toString());
+		a.multi(b);
+		System.out.print("Matrix a*b:\n" + a.toString());
 
-		// int m4[][] = { { 1, -2 }, { -4, 3} };
-		// Matrix d = new Matrix(m4);
-		// System.out.println("Value of det:\n" + d.detValue());
+		System.out.println("a的转置矩阵：\n" + a.transpose().toString());
 
-		// int m5[][] = { { 1, 1, 1 }, { 0, 1, 2 }, { 0, 1, -3 } };
-		// Matrix e = new Matrix(m5);
-		// System.out.println("伴随矩阵:\n" + e.ajoint().toString());
+		float m3[][] = { { 1, 2, -3 }, { 0, 1, 2 }, { 0, 0, 1 } };
+		Matrix c = new Matrix(m3);
+		System.out.println("Value of det:\n" + c.detCofactor(3, 3).toString());
 
-		int m6[][] = { { 1, 2, -3 }, { 0, 1, 2 }, { 0, 0, 1 } };
+		float m4[][] = { { 1, -2 }, { -4, 3 } };
+		Matrix d = new Matrix(m4);
+		System.out.println("Value of det:\n" + d.detValue());
+
+		float m5[][] = { { 1, 2, 3 }, { 2, 3, 4 }, { 3, 4, 5 } };
+		Matrix e = new Matrix(m5);
+		System.out.println("伴随矩阵:\n" + e.ajoint().toString());
+
+		float m51[][] = { { 1, 2 }, { 3, 4 } };
+		Matrix e1 = new Matrix(m51);
+		System.out.println("伴随矩阵:\n" + e1.ajoint().toString());
+
+		float m6[][] = { { 1, 2, -3 }, { 0, 1, 2 }, { 0, 0, 1 } };
 		Matrix f = new Matrix(m6);
-		System.out.println("逆矩阵:\n" + f.ajoint().toString());
+		System.out.println("逆矩阵:\n" + f.reverse().toString());
+
+		float m7[][] = { { 0, 1 }, { -1, 0 } };
+		Matrix g = new Matrix(m7);
+		System.out.println("逆矩阵:\n" + g.reverse().toString());
+
+		// 验证 “矩阵伴随的转置等于矩阵转置的伴随”
+		float m8[][] = { { 1, 2, -3 }, { 0, 1, 2 }, { 0, 0, 1 } };
+		Matrix h = new Matrix(m8);
+		System.out.println("矩阵伴随的转置:\n" + h.ajoint().transpose().toString());
+		System.out.println("矩阵转置的伴随:\n" + h.transpose().ajoint().toString());
 
 	}
 }
